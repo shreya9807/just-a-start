2ac01bc0a408330ed9a741040729cbe275f77153
shreya9807 <das.shreya0798@gmail.com>
2025-08-05 22:46:58 +0530

    Initial commit 1

---
+++ bookstore/src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -0,0 +1,14 @@
+package com.example.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
